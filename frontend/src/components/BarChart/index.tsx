137953d394fd12f37067125010ce039a26c27c41
Alef Ferraz <alef.ferraz00@gmail.com>
2021-05-08 12:45:19 -0300

    Static charts

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,5 +1,5 @@
-import Chart from 'react-apexcharts'
-function BarChart() {
+import Chart from 'react-apexcharts';
+const BarChart = () => {
 
     const options = {
         plotOptions: {
@@ -8,7 +8,7 @@ function BarChart() {
             }
         },
     };
-    
+
     const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -16,21 +16,24 @@ function BarChart() {
         series: [
             {
                 name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]
             }
         ]
     };
+
+
     return (
-        <Chart 
-        option={{...options, xaxis: mockData.labels}} 
-        series={mockData.series}
-        type="bar"
-        height="240"       
+
+        <Chart  
+          options={{ ...options, xaxis: mockData.labels}}
+          series={mockData.series}
+          type="bar"
+          height="240"
         />
-      
-            );
-        }
-
-
+     
+  
+  
+    );
+}
 
 export default BarChart;
