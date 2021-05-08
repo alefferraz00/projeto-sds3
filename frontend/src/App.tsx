1415a38842afc56ca396c42a3c44f7ec3cec2dd4
Alef Ferraz <alef.ferraz00@gmail.com>
2021-05-08 12:00:15 -0300

    Correção no frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
-import DataTable from "./components/DataTable";
-import Footer from "./components/Footer";
+
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary">Olá mundo!</h1>
 
-        <DataTable/>
-        
-      </div>
-      <Footer/>
+    <>
+    <NavBar />
+    <div className="container"> 
+    <h1 className="text-primary">Olá mundo!</h1>
+    <DataTable />
+    </div>
+    <Footer />
+
     </>
+
   );
 }
 
